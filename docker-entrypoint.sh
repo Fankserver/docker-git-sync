61d7bcfdf24b88100812fd17c28d1f2455af3366
Florian Kinder <florian.kinder@fankserver.com>
2017-06-20 21:35:45 +0200

    Fixed repo mirror

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 cd /mnt
 git clone --bare $ORIGIN_URL
 cd *
-git remote add --mirror=push mirror $MIRROR_URL
-git fetch mirror
+git remote add mirror $MIRROR_URL
+
+# Initial run
+/etc/periodic/15min/sync.sh
 
 crond -f
--- sync.sh
+++ sync.sh
@@ -6,27 +6,17 @@ if [ "$?" != "0" ]; then
   echo "ERROR: Repo does not exists"
   exit 1
 fi
-git fetch origin
+git fetch origin --prune
 if [ "$?" != "0" ]; then
   echo "ERROR: failed fetch from $ORIGIN_URL"
   exit 1
 fi
-git fetch origin --tags
+git fetch origin --prune --tags
 if [ "$?" != "0" ]; then
   echo "ERROR: failed fetch from $ORIGIN_URL"
   exit 1
 fi
-git fetch mirror
-if [ "$?" != "0" ]; then
-  echo "ERROR: failed fetch from $MIRROR_URL"
-  exit 1
-fi
-git fetch mirror --tags
-if [ "$?" != "0" ]; then
-  echo "ERROR: failed fetch from $MIRROR_URL"
-  exit 1
-fi
-git push mirror
+git push --force --tags mirror refs/heads/*:refs/heads/*
 if [ "$?" != "0" ]; then
   echo "ERROR: failed push to $MIRROR_URL"
   exit 1
